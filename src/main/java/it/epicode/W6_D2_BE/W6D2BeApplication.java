4e67ff20677ea153478ee28560e2613e426207b3
Vincenza <vinni2690@hotmail.com>
2025-06-10 15:31:38 +0200

    Initial commit

---
+++ src/main/java/it/epicode/W6_D2_BE/W6D2BeApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.W6_D2_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W6D2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W6D2BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/W6_D2_BE/W6D2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.W6_D2_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6D2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
